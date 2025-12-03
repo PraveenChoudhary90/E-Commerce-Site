c968260f3e6412d9774bd9fcd6191f229ac0d382
Praveen Choudhary <pc852180@gmail.com>
2025-12-03 16:52:03 +0530

    Memeber Insert and Display

--- src/constants/mainContent.js
+++ src/constants/mainContent.js
@@ -19,7 +19,7 @@ export const backendConfig = {
 
 const token = localStorage.getItem("token");
 export const Axios = axios.create({
-  baseURL:"http://localhost:8000",
+  baseURL:"http://localhost:5000",
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${token}`,
